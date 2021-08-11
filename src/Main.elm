c73f3b7da7cc0e7e0341c1ac07a3459556ef9f39
Corey Haines <coreyhaines@gmail.com>
2021-08-11 13:08:41 -0500

    Task list and task input box

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,9 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, div, text)
+import Html exposing (Html, button, div, input, text)
+import Html.Attributes exposing (value)
+import Html.Events exposing (onInput)
 
 
 type alias Flags =
@@ -9,37 +11,83 @@ type alias Flags =
 
 
 type alias Model =
-    {}
-
-
-type Msg
-    = Noop
-
-
-init : Flags -> ( Model, Cmd Msg )
-init _ =
-    ( {}, Cmd.none )
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update _ model =
-    ( model, Cmd.none )
-
-
-view : Model -> Browser.Document Msg
-view _ =
-    { title = "Hello, Elm!"
-    , body = [ div [] [ text "Hello, eSpark! Let's build a TODO app!" ] ]
+    { tasks : List Task
+    , newTaskName : String
     }
 
 
-{-| Browser.document : { init : Flags -> ( Model, Cmd Msg )
-, update : Msg -> Model -> ( Model, Cmd Msg )
-, view : Model -> Document Msg
-, subscriptions : Model -> Sub Msg
-} -> Program Flags Model Msg
+type alias Task =
+    { name : String
+    }
+
+
+type Message
+    = NewTaskNameInput String
+
+
+
+-- Type inference
+
+
+initialTasks =
+    [ { name = "Clean my fridge" }
+    , { name = "Feed cats" }
+    , { name = "Change cat's water" }
+    ]
+
+
+init : Flags -> ( Model, Cmd Message )
+init _ =
+    ( { tasks = initialTasks
+      , newTaskName = ""
+      }
+    , Cmd.none
+    )
+
+
+update : Message -> Model -> ( Model, Cmd Message )
+update msg model =
+    case msg of
+        NewTaskNameInput userInput ->
+            ( { model
+                | newTaskName = userInput
+              }
+            , Cmd.none
+            )
+
+
+view : Model -> Browser.Document Message
+view model =
+    { title = "Hello, Elm!"
+    , body =
+        [ div [] [ text "Hello, FOLKS! Let's build a TODO app!" ]
+        , addNewTaskView model
+        , taskListView model
+        ]
+    }
+
+
+addNewTaskView model =
+    div []
+        [ input [ onInput NewTaskNameInput ] []
+        ]
+
+
+taskListView model =
+    div [] (List.map taskView model.tasks)
+
+
+taskView task =
+    div [] [ text task.name ]
+
+
+{-| Browser.document : { init : Flags -> ( Model, Cmd Message )
+, update : Message -> Model -> ( Model, Cmd Message )
+, view : Model -> Document Message
+, subscriptions : Model -> Sub Message
+} -> Program Flags Model Message
 -}
-main : Program Flags Model Msg
+main : Program Flags Model Message
 main =
     Browser.document
         { init = init
